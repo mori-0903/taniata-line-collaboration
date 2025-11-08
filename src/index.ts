bc16a61bafa5c4d8886578a82274cd2dab481936
morio <moriforest537@gmail.com>
2025-11-08 16:46:53 +0900

    情報取得できるようになるところまで

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { fetchWeight } from "./getWeight";
+
+require("dotenv").config();
+
+async function index() {
+	const weightObject = await fetchWeight();
+	console.log(weightObject);
+}
+
+index();
