38d84af0e01b1ecfb4723d16760ea8f0d2db83be
morio <moriforest537@gmail.com>
2025-11-08 20:25:15 +0900

    本日体重測ってなければ通知しない

--- src/index.ts
+++ src/index.ts
@@ -9,24 +9,26 @@ require("dotenv").config()
 async function index() {
   const fetchData = await fetchWeight()
   const { currentWeight, meanWeight } = processWeightData(fetchData)
-  const today = dayjs().tz("Asia/Tokyo").format("YYYY年MM月DD日")
-  const spleadsheetUrl =
-    "https://docs.google.com/spreadsheets/d/1-R4P51Zy_WFRKIliaXERNq2hiON26hRVVf4hhKNxjXQ/edit?usp=sharing"
-  const messageObject: LINEMessage[] = [
-    {
-      type: "text",
-      text: `転記先スプレッドシート \n${spleadsheetUrl}`,
-    },
-    {
-      type: "text",
-      text: `ここ2週間の平均体重: ${meanWeight}kg \n${today}の最新体重(kg):`,
-    },
-    {
-      type: "text",
-      text: currentWeight ? `${currentWeight}` : "データがありません",
-    },
-  ]
-  noticeLINEMessage(messageObject)
+  if (currentWeight) {
+    const today = dayjs().tz("Asia/Tokyo").format("YYYY年MM月DD日")
+    const spleadsheetUrl =
+      "https://docs.google.com/spreadsheets/d/1-R4P51Zy_WFRKIliaXERNq2hiON26hRVVf4hhKNxjXQ/edit?usp=sharing"
+    const messageObject: LINEMessage[] = [
+      {
+        type: "text",
+        text: `転記先スプレッドシート \n${spleadsheetUrl}`,
+      },
+      {
+        type: "text",
+        text: `ここ2週間の平均体重: ${meanWeight}kg \n${today}の最新体重(kg):`,
+      },
+      {
+        type: "text",
+        text: currentWeight ? `${currentWeight}` : "データがありません",
+      },
+    ]
+    noticeLINEMessage(messageObject)
+  }
 }
 
 index()
